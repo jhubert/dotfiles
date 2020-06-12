8057819229c23b37871a8ad30628687913b68865
Jeremy Baker <jeremy@retailzipline.com>
2020-06-11 19:45:16 -0500

    Add the z file navigation command

--- init.sh
+++ init.sh
@@ -9,3 +9,6 @@ source /Users/jhubert/.dotfiles/git/git-prompt.sh
 # Ruby
 source /Users/jhubert/.dotfiles/ruby/aliases.sh
 source /Users/jhubert/.dotfiles/ruby/shell.sh
+
+# Z - Quick folder navigation helper
+source /Users/jhubert/.dotfiles/z/z.sh
